48d347efc0886c35dd15113e6d6e30f94fd378cb
Tully Foote <tfoote@osrfoundation.org>
2014-12-29 22:05:13 -0800

    removing echo of module path

--- deploy.bash
+++ deploy.bash
@@ -6,5 +6,4 @@ cp hiera.yaml /etc/puppet
 mkdir -p /etc/puppet/hieradata
 cp common.yaml /etc/puppet/hieradata
 
-puppet module --modulepath=/etc/puppet/modules:/usr/share/puppet/modules:`pwd` list
 puppet apply -v site.pp --modulepath=/etc/puppet/modules:/usr/share/puppet/modules:`pwd` #-l /var/log/puppet.log
