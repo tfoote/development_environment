dc2c7f71c386fe434bbf99c17f41d70fdb1dd50d
Tully Foote <tfoote@osrfoundation.org>
2015-01-02 14:49:22 -0800

    rerunning librarian in case a new module is needed.

--- deploy.bash
+++ deploy.bash
@@ -6,4 +6,5 @@ cp hiera.yaml /etc/puppet
 mkdir -p /etc/puppet/hieradata
 cp common.yaml /etc/puppet/hieradata
 
+librarian-puppet install --verbose
 puppet apply -v site.pp --modulepath=/etc/puppet/modules:/usr/share/puppet/modules:`pwd`:`pwd`/modules #-l /var/log/puppet.log
--- install_prerequisites.bash
+++ install_prerequisites.bash
@@ -5,5 +5,3 @@ set -o errexit
 apt-get update
 apt-get install -y ruby ruby1.9.1-dev
 gem install puppet librarian-puppet --no-rdoc --no-ri
-
-librarian-puppet install --verbose
