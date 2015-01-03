30bffb8dce358012c1bc364b962ecb5b6aca594b
Tully Foote <tfoote@osrfoundation.org>
2015-01-03 15:52:56 -0800

    configure librarian-puppet to use rsync for higher efficiency

--- install_prerequisites.bash
+++ install_prerequisites.bash
@@ -5,3 +5,6 @@ set -o errexit
 apt-get update
 apt-get install -y ruby ruby1.9.1-dev
 gem install puppet librarian-puppet --no-rdoc --no-ri
+
+# config for librarian for more efficient syncronization
+librarian-puppet config rsync true --global
