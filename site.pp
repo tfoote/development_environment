ae1ebea7da377f17d5f76387587f270a5c354826
Tully Foote <tfoote@osrfoundation.org>
2016-01-05 16:01:22 -0800

    add mounting of squid logs for debugging purposes

--- site.pp
+++ site.pp
@@ -383,6 +383,13 @@ if hiera('run_squid', false) {
     owner  => 'proxy',
     group  => 'proxy',
   }
+  
+  file { '/var/log/squid-in-a-can' :
+    ensure => 'directory',
+    mode   => 644,
+    owner  => 'proxy',
+    group  => 'proxy',
+  }
 
   docker::run {'squid-in-a-can':
     image   => 'jpetazzo/squid-in-a-can',
@@ -392,10 +399,12 @@ refresh_pattern . 0 0 1 refresh-ims
 refresh_all_ims on # make sure we do not get out of date content
 ignore_expect_100 on # needed for new relic system monitor
 \''],
-    volumes => ['/var/cache/squid-in-a-can:/var/cache/squid3'],
+    volumes => ['/var/cache/squid-in-a-can:/var/cache/squid3',
+                '/var/log/squid-in-a-can:/var/log/squid3'],
     net     => 'host',
     require => [Docker::Image['jpetazzo/squid-in-a-can'],
                 File['/var/cache/squid-in-a-can'],
+                File['/var/log/squid-in-a-can'],
                ],
   }
 
