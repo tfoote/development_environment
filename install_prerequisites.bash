55b7602e2ea314df9b271cf92f4c2bb526015673
Tully Foote <tfoote@osrfoundation.org>
2016-07-06 13:19:03 -0700

    update for xenial

--- install_prerequisites.bash
+++ install_prerequisites.bash
@@ -3,8 +3,7 @@
 set -o errexit
 
 apt-get update
-apt-get install -y ruby ruby1.9.1-dev
-gem install puppet librarian-puppet --no-rdoc --no-ri
+apt-get install -y ruby ruby-dev librarian-puppet
 
 # config for librarian for more efficient syncronization
 librarian-puppet config rsync true --global
